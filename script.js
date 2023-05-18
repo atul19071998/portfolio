e5171b32144dfb4bbcfae1dbff8c352ea14b785f
Atul <ajayc5314@gmail.com>
2023-05-19 01:32:34 +0530

    new updates

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //toggle icon navbar 
 
-let menuIcon =  document.getElementById('menu-icon');
+let menuIcon =  document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () =>{
@@ -38,7 +38,7 @@ window.onscroll =() =>{
     // sticky navbar
     let header = document.querySelector('header');
 
-    header.classList.toogle('sticky ', window.scrollY > 100);
+    header.classList.toggle('sticky', window.scrollY > 100);
 
     // remove toogle icon and 
     
@@ -72,3 +72,21 @@ window.onscroll =() =>{
       loop:true
 
     })
+
+
+    let options = {
+        startAngle: -1.55,
+        size: 150,
+        value: 0.85,
+        fill: {gradient: ['#a445b2', '#fa4299']}
+      }
+      $(".circle .bar").circleProgress(options).on('circle-animation-progress',
+      function(event, progress, stepValue){
+        $(this).parent().find("span").text(String(stepValue.toFixed(2).substr(2)) + "%");
+      });
+      $(".js .bar").circleProgress({
+        value: 0.70
+      });
+      $(".react .bar").circleProgress({
+        value: 0.60
+      });
